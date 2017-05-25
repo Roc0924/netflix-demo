9cda9dd1a2fde2a79f648a13f4573065f7e1bbbd
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-05-25 10:49:27 +0800

    commit

--- config-server/src/main/java/com/example/demo/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
