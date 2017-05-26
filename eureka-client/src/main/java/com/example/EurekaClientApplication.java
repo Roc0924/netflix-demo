0738d1c6650758d1f7f67b33b56a85ad4f2e126a
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-05-26 14:42:10 +0800

    commit

--- eureka-client/src/main/java/com/example/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/EurekaClientApplication.java
@@ -2,12 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(EurekaClientApplication.class).web(true).run(args);
+		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 }
